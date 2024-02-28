022b462a84e0bc2fbdbecbbe466f203216d3d65b
Shiv Narayanan <shiv.narayanan@icloud.com>
2024-02-28 16:04:16 +0800

    chore: more update.py to src

--- src/update.py
+++ src/update.py
@@ -8,7 +8,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 load_dotenv(override=True)
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('google-credentials.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('./google-credentials.json', scope)
 client = gspread.authorize(credentials)
 
 DATABASE_URL = os.environ['DATABASE_URL']  
